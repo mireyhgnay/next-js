23015cd2345f4cc001fb18b4140236d0b2871385
hyerim <hyerimiya1216@gmail.com>
2024-12-24 18:29:24 +0900

    Feat: Sever Component Data Fetching & Loading Components

---
+++ app/(movies)/movies/[id]/loading.tsx
@@ -0,0 +1,3 @@
+export default function Loading() {
+  return <h3>Movie Detail Loading...</h3>;
+}
--- app/(movies)/movies/[id]/page.tsx
+++ app/(movies)/movies/[id]/page.tsx
@@ -1,7 +1,25 @@
-export default function MovieDetail({
+import { API_URL } from '../../../(home)/page';
+
+async function getMovie(id: string) {
+  console.log(`Fetching movies: ${Date.now()}`);
+  const response = await fetch(`${API_URL}/${id}`);
+  return response.json();
+}
+async function getVideos(id: string) {
+  console.log(`Fetching videos: ${Date.now()}`);
+  const response = await fetch(`${API_URL}/${id}/videos`);
+  return response.json();
+}
+
+export default async function MovieDetail({
   params: { id },
 }: {
   params: { id: string };
 }) {
-  return <h1>Movie {id}</h1>;
+  const [movie, videos] = await Promise.all([getMovie(id), getVideos(id)]); // data fetching 병렬로 진행
+  return (
+    <>
+      <h1>{movie.title}</h1>
+    </>
+  );
 }
